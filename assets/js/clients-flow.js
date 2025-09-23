828a77292edadc3c467a83422a35935376a79818
iShenshen <markbakulaw123@gmail.com>
2025-09-23 11:52:46 +0800

    first commit

---
+++ assets/js/clients-flow.js
@@ -0,0 +1,5 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const marqueeTrack = document.querySelector(".marquee-track");
+//     const slides = marqueeTrack.innerHTML; 
+//     marqueeTrack.innerHTML += slides; // duplicate logos
+//   });
